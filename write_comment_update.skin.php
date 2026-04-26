579f026cb8b115e6b8296fb448fa34fdd78bea24
jo066 <50199059+cuplecter19@users.noreply.github.com>
2026-04-26 15:32:46 +0900

    Add files via upload

---
+++ write_comment_update.skin.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
+
+// 자신만의 코드를 넣어주세요.
+// 주의: 여기서 goto_url()을 호출하면 write_comment_update.php의
+// 댓글 앵커(#c_) 리다이렉트가 실행되지 않습니다.
+// 댓글 위치로 이동이 필요하면 이 파일에서 goto_url()을 사용하지 마세요.
+// goto_url("./board.php?bo_table=$bo_table&page=$page");
+?>
